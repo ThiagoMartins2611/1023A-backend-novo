96825b9f6be329152098ba19810960c7fc6cccc3
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-08-26 08:30:50 -0400

    MAIS CONFIGURAÇÕES DAS VARIAVEIS de ambiente

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import 'dotenv/config'
+import mysql from 'mysql2/promise'
 
-import express, { type Response } from 'express';
+import express, { Response, Request } from 'express';
 const app = express();
 
 
-app.get('/', (req:string, res:any)=>{
-    res.send(process.env.DBUSER)
+app.get('/', async (req:Request, res:Response)=>{
+    try {
+
+        const connection = await mysql.createConnection({
+            host: process.env.DBHOST?process.env.DBHOST:'error localhost',
+            user: process.env.DBUSER?process.env.DBUSER:'error user',
+            password: process.env.DBPASSWORD?process.env.DBPASSWORD:'',
+            database: process.env.DBNAME?process.env.DBNAME:"error name",
+            port: Number(process.env.DBPORT?process.env.DBPORT:"3306")
+        })
+        res.send("Conectado ao banco de dados com sucesso");
+
+        await connection.end();
+        
+    } catch (error) {
+        res.status(500).send("Erro ao conectar ao banco de dados: "+ error);
+    }
+
 });
 
 app.listen(8000,()=>{
