14e8c47f825dca28af460ca3c23130cf7233e01f
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-09-02 08:22:55 -0400

    questão para a proxima aula

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,15 @@ app.get('/', async (req:Request, res:Response)=>{
 
 });
 
+app.get('/produtos', async (req:Request, res:Response)=>{
+
+});
+
 app.listen(8000,()=>{
     console.log("SERVER RUNNING IN THE PORT 8000, http://localhost:8000");
 })
+
+//CRIAR UMA ROTA GET PARA PRODUTOS QUE RETORNE A LISTA DE PRODUTOS DO BANCO DE DADOS
+//O produto deve ter id, nome, preco, urlfoto, descricao
+//deve se criar uma tabela no banco de dados AIVEN para armazenar os produtos
+//A respsota deve ser uma array de produtos no formato JSON
