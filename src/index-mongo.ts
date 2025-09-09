9b5a474fd3606732bfd021c260eb7d259a648e4d
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-09-09 11:28:49 -0400

    Funcionando com o mongo db

---
+++ src/index-mongo.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express, { Response, Request } from 'express';
+import {MongoClient} from 'mongodb';
+
+const client = new MongoClient(process.env.MONGO_URI!);
+await client.connect();
+const db = client.db(process.env.MONGO_DB);
+
+const app = express();
+app.use(express.json())
+
+app.get('/estudantes', async (req:Request, res:Response)=>{
+    const estudantes = await db.collection('estudantes').find().toArray();
+    res.status(200).json(estudantes);
+});
+
+
+app.listen(8000,()=>{
+    console.log("SERVER RUNNING IN THE PORT 8000, http://localhost:8000/estudantes");
+})
--- src/index.ts
+++ src/index.ts
@@ -47,22 +47,3 @@ app.get('/produtos', async (req:Request, res:Response)=>{
 app.listen(8000,()=>{
     console.log("SERVER RUNNING IN THE PORT 8000, http://localhost:8000");
 })
-
-//CRIAR UMA ROTA GET PARA PRODUTOS QUE RETORNE A LISTA DE PRODUTOS DO BANCO DE DADOS
-//O produto deve ter id, nome, preco, urlfoto, descricao
-//deve se criar uma tabela no banco de dados AIVEN para armazenar os produtos
-//A respsota deve ser uma array de produtos no formato JSON
-/*
-
-CREATE TABLE produtos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    preco DECIMAL(10,2) NOT NULL,
-    urlfoto VARCHAR(255) NOT NULL,
-    descricao TEXT
-)
- 
-Faz pelo menos 3 inserts
-
-
-*/ 
