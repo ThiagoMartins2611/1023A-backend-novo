62512622d4b32a8f91e8a52887c667331c8d3e04
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-10-14 11:51:31 -0400

    Auth infertace finalizado, no entanto temos um bug no payload

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,37 @@
 import { Request, Response, NextFunction } from "express";
+import  jwt  from "jsonwebtoken";
+
+interface AuthRequest extends Request{
+    usuarioId?:string
+}
 
 class Auth{
 
-    userAuth(req:Request, res:Response, next: NextFunction){
-        console.log("passei no middleware")
-        next()
+    userAuth(req:AuthRequest, res:Response, next: NextFunction){
         
+        const authUserHeader = req.headers.authorization
+
+        if(!authUserHeader){
+            return res.status(401).json({mensagem:"token não fornecido"});
+        }
+
+        const token = authUserHeader.split(" ")[1]!
+        jwt.verify(token, process.env.JWT_SECRET!, (err,decoded)=>{
+
+            if(err){
+                console.log(err);
+                return res.status(401).json({mensagem:"Token invalido"});
+            }
+
+            if(typeof decoded === "string" || !decoded || !("usuarioId" in decoded)){
+                return res.status(401).json({mensagem:"PayLoad invalido"});
+            }
+
+            req.usuarioId = decoded.usuarioId;
+
+            next()
+        });
+
     }
 
 }
